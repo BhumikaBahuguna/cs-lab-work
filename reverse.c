0135c9ff3569cb210cc62e3753b2e31ccd30f955
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-02-06 13:15:28 +0530

    reverse.c

--- reverse.c
+++ reverse.c
@@ -1,5 +1,3 @@
-BASIC:
-
 REVERSE AN ARRAY........
 
 #include<stdio.h>
@@ -21,48 +19,6 @@ int main()
         }
     return 0;  
     }
-
-
-
-INPUT N ELEMENTS AND PRINT PALINDROME ELEMENTS......
-
-
-#include <stdio.h>
-int isPalindrome(int ) ;
-int main() {
-    int n, i;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-    int arr[n];
-
-    printf("Enter %d elements:\n", n);
-    for(i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    printf("Palindrome elements in the array are:\n");
-    for(i = 0; i < n; i++) {
-        if(isPalindrome(arr[i])) {
-            printf("%d\n", arr[i]);
-        }
-    }
-
-    return 0;
-}
-int isPalindrome(int num) {
-    int reversed = 0, original = num;
-    while (num != 0) {
-        reversed = reversed * 10 + num % 10;
-        num /= 10;
-    }
-    return original == reversed;
-}
-
-
-
-
-
-
 
 
 1)To arrange elements of all rows in ascending order 
