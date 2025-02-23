93f6544d68ea48b5ca58dafd9d5fa26219c2f593
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-02-23 23:24:51 +0530

    Update lower_to_upper.c

--- lower_to_upper.c
+++ lower_to_upper.c
@@ -28,40 +28,6 @@ NOTE: (The <ctype> header file in C defines various functions to classify and tr
 
 
 
-15)REPLACE 'a' with $
-
-
-
-#include <stdio.h>
-
-int main() {
-    char str[100];
-    
-    // Input the string
-    printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin);
-    
-    // Replace 'a' with '$'
-    for(int i = 0; str[i] != '\0'; i++) {
-        if(str[i] == 'a') {
-            str[i] = '$';
-        }
-        // Check for newline character and replace it with null terminator
-        if(str[i] == '\n') {
-            str[i] = '\0';
-            break;
-        }
-    }
-    
-    // Output the modified string
-    printf("Modified string: %s\n", str);
-    
-    return 0;
-}
-
-
-
-
 
 16) STRING PALINDROME OR NOT 
 
